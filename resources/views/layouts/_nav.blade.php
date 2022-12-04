84cffc8e5ddf9c6258c5b7b2f519ca31947cb745
Oscar <1930672@upv.edu.mx>
2022-12-04 15:49:37 -0600

    done

---
+++ resources/views/layouts/_nav.blade.php
@@ -0,0 +1,75 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item nav-profile">
+            <div class="nav-link">
+                <div class="profile-image">
+                    <img src="{{asset('melody/images/faces/face24.png')}}" alt="image" />
+                </div>
+                <div class="profile-name">
+                    <p class="name">
+                        {{ Auth::user()->name }}
+                    </p>
+                    <p class="designation">
+                        {{ Auth::user()->email }}
+                    </p>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('home')}}">
+                <i class="fa fa-home menu-icon"></i>
+                <span class="menu-title">Dashboard</span>
+            </a>
+        </li>
+       
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('purchases.index')}}">
+                <i class="fas fa-cart-plus menu-icon"></i>
+                <span class="menu-title">Compras</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('sales.index')}}">
+                <i class="fas fa-shopping-cart menu-icon"></i>
+                <span class="menu-title">Ventas</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('categories.index')}}">
+                <i class="fas fa-tags menu-icon"></i>
+                <span class="menu-title">Categorías</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('products.index')}}">
+                <i class="fas fa-boxes menu-icon"></i>
+                <span class="menu-title">Productos</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('clients.index')}}">
+                <i class="fas fa-users menu-icon"></i>
+                <span class="menu-title">Clientes</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('providers.index')}}">
+                <i class="fas fa-shipping-fast menu-icon"></i>
+                <span class="menu-title">Proveedores</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('users.index')}}">
+                <i class="fas fa-user-tag menu-icon"></i>
+                <span class="menu-title">Usuarios</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('roles.index')}}">
+                <i class="fas fa-user-cog menu-icon"></i>
+                <span class="menu-title">Roles</span>
+            </a>
+        </li>
+    </ul>
+</nav>
